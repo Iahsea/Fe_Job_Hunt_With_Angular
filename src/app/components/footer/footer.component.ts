13dff5262d89517a4ac9cf75f801b8a1ab32fbdd
dovankhang <khangdv@blueoath.jp>
2025-08-27 23:12:56 +0700

    feat: UI header and footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { environment } from '../../environments/environments';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  public backendUrl = environment.imagesUrl;
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { environment } from '../../environments/environments';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  public backendUrl = environment.imagesUrl;
 }
